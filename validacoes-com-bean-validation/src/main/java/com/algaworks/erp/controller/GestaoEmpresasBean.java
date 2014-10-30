c3646b70f190a14d67ed48453d077b891a6698ef
Thiago Faria de Andrade <thiagofa@gmail.com>
2014-10-30 01:47:41 -0300

    Aula sobre validações com Bean Validation

---
+++ validacoes-com-bean-validation/src/main/java/com/algaworks/erp/controller/GestaoEmpresasBean.java
@@ -0,0 +1,65 @@
+package com.algaworks.erp.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import com.algaworks.erp.model.Empresa;
+import com.algaworks.erp.model.TipoEmpresa;
+import com.algaworks.erp.repository.Empresas;
+import com.algaworks.erp.service.CadastroEmpresaService;
+import com.algaworks.erp.util.FacesMessages;
+
+@Named
+@ViewScoped
+public class GestaoEmpresasBean implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private Empresas empresas;
+	
+	@Inject
+	private CadastroEmpresaService cadastroEmpresa;
+	
+	@Inject
+	private FacesMessages messages;
+	
+	private List<Empresa> todasEmpresas;
+	private Empresa empresaEdicao = new Empresa();
+	
+	public void prepararNovoCadastro() {
+		empresaEdicao = new Empresa();
+	}
+	
+	public void salvar() {
+		cadastroEmpresa.salvar(empresaEdicao);
+		consultar();
+		
+		messages.info("Empresa salva com sucesso!");
+	}
+	
+	public void consultar() {
+		todasEmpresas = empresas.todas();
+	}
+
+	public List<Empresa> getTodasEmpresas() {
+		return todasEmpresas;
+	}
+	
+	public TipoEmpresa[] getTiposEmpresas() {
+		return TipoEmpresa.values();
+	}
+
+	public Empresa getEmpresaEdicao() {
+		return empresaEdicao;
+	}
+
+	public void setEmpresaEdicao(Empresa empresaEdicao) {
+		this.empresaEdicao = empresaEdicao;
+	}
+	
+}
